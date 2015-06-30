9c93e94fb2151f6efb399a0d6dc5bce9752fa59a
David Colgan <dvcolgan@gmail.com>
2015-06-30 14:24:21 -0400

    added lots of ideas

--- scenarios/prisonship/items.lua
+++ scenarios/prisonship/items.lua
@@ -1,2 +1,25 @@
 return {
+    {
+        id = 1,
+        name = 'Pin Cannon',
+        armor = 1,
+        weight = 1,
+        size = 3,
+        power = 100,
+        cooling = 0,
+        mount = 'shoulder',
+        attrs = {
+            'piercing',
+        },
+    },
+    {
+        id = 2,
+        name = 'Arm Cannon',
+        armor = 2,
+        weight = 5,
+        size = 6,
+        power = 200,
+        mount = 'arm',
+        attrs = {}
+    },
 }
