d50a6758564c9e4f26d0ed9e95e7d64031f57e72
David Colgan <dvcolgan@gmail.com>
2015-07-01 15:14:39 -0400

    working on stuff

--- models/map.lua
+++ models/map.lua
@@ -45,6 +45,7 @@ end
 function Map:chooseRandomRoom(roomType)
     local roomsOfType = self.roomTemplates[roomType]
     local index = math.random(#roomsOfType)
+    index = 1
     return roomsOfType[index], index
 end
 
